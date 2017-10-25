867468cb4dce993b2245d82f78c6523cb73cb79e
murog <guillermina.muro@gmail.com>
2017-10-25 15:35:40 -0700

    updated application helpers to include date and money format methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def readable_date(date)
+    ("<span class='date'>" + date.strftime("%A, %b %d") + "</span>").html_safe
+  end
+
+
+
+  def readable_money(money)
+    #TODO: helper method that will format cost and revenue appropriate
+    #10 =>  $10.00
+    #10.0 => $10.00
+    #10.00 => $10.00
+    #10.3333 => $10.33
+  end
+
+
 end
