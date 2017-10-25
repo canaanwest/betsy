148c7c6e7a462fde349e6923379d76f8dc457d89
Canaan West <canaan.west@gmail.com>
2017-10-25 14:17:35 -0700

    merging conflicts in tests

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -33,17 +33,6 @@ describe ItemsController do
     end
   end
 
-
-<<<<<<< HEAD
-  # TODO
-  describe "Create" do
-    it "Should be able to create a new item" do
-      proc {
-        post create_item_path, params: {item: {shipping_status: false, purchase_status: false, product_id: products[:converse].id, order_id: nil }}}.must_change 'Item.count', 1
-
-        must_respond_with :redirect
-        must_redirect_to root_path
-=======
   describe "Create" do
     it "Should be able to create a new item" do
 
@@ -55,9 +44,6 @@ describe ItemsController do
 
       must_respond_with :redirect
       must_redirect_to root_path
-
-
->>>>>>> 3cfed66259465437ed4cd847fb98d29746334c09
     end
   end
 
