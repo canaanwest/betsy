de91a01356d02ca64d03f9f88ca6332615a43836
Julia <julia.meier@gmail.com>
2017-10-25 17:49:32 -0700

    migrated project_photo. Displays on site.

--- db/migrate/20171025212948_add_photo_to_products.rb
+++ db/migrate/20171025212948_add_photo_to_products.rb
@@ -1,5 +1,5 @@
 class AddPhotoToProducts < ActiveRecord::Migration[5.1]
   def change
-    add_column :products, :product_photo, :string, :default => "http://fillmurray.com/g/300/300"
+    add_column :products, :product_photo, :string, :default => "https://i.imgur.com/M8g4Cdu.jpg?1"
   end
 end
