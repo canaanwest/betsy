76de44a20915f134aba53954d760a70ca7263f7d
Canaan West <canaan.west@gmail.com>
2017-10-27 12:57:25 -0700

    final deployment for show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,25 @@
 module ApplicationHelper
   def readable_date(date)
-    ("<span class='date'>" + date.strftime("%A, %b %d") + "</span>").html_safe
+    ("<span class='date'>" + date.strftime("%b %d %Y") + "</span>").html_safe
   end
 
-
-
   def readable_money(money)
-    #TODO: helper method that will format cost and revenue appropriate
-    #10 =>  $10.00
+    #10 => $10.00
     #10.0 => $10.00
     #10.00 => $10.00
     #10.3333 => $10.33
+    if !money
+      return "$0.00"
+    end
+    money = money.to_s
+    if money[-3] == "."
+      return "$#{money}"
+    elsif money[-2..-1] == ".0"
+      return "$#{money}0"
+    elsif
+      money[-3..-1] == ".00"
+      return "$#{money}"
+    else
+    end
   end
-
-
 end
