5bcb6fc2123a70ae67f3dda6a7a59f3e581c69af
murog <guillermina.muro@gmail.com>
2017-10-27 00:35:18 -0700

    3 out of 4 passing tests, thinking test 2 should-return-404-if-items-are-already-shipped is not necessary

--- test/controllers/order_products_controller_test.rb
+++ test/controllers/order_products_controller_test.rb
@@ -12,6 +12,63 @@ describe OrderProductsController do
       delete order_product_path(entry)
       must_respond_with :redirect
     end
+  describe "ship" do
+    it "should return 404 if entry does not exist" do
+
+      post ship_path(100000)
+      must_respond_with :not_found
+    end
+    it "should return 404 if the items for that entry are already shipped" do
+      shipped_order = orders(:shipped_order)
+      product = products(:chocolate)
+      one_single_chocolate = Item.new
+      one_single_chocolate.order = shipped_order
+      one_single_chocolate.product = product
+      one_single_chocolate.shipping_status = true
+      one_single_chocolate.save
+      entry = OrderProduct.new
+      entry.product = product
+      entry.order = shipped_order
+      entry.quantity = 1
+      entry.save
+      # binding.pry
+      post ship_path(entry.id)
+      must_respond_with :not_found
+    end
+    it "should redirect to order_fulfillment if successful" do
+      shipped_order = orders(:shipped_order)
+      product = products(:chocolate)
+      one_single_chocolate = Item.new
+      one_single_chocolate.order = shipped_order
+      one_single_chocolate.product = product
+      one_single_chocolate.save
+      entry = OrderProduct.new
+      entry.product = product
+      entry.order = shipped_order
+      entry.quantity = 1
+      entry.save
+
+      post ship_path(entry.id)
+      must_respond_with :redirect
+    end
+
+    it "should respond with success if items get shipped" do
+      shipped_order = orders(:shipped_order)
+      product = products(:chocolate)
+      one_single_chocolate = Item.new
+      one_single_chocolate.order = shipped_order
+      one_single_chocolate.product = product
+      one_single_chocolate.save
+      entry = OrderProduct.new
+      entry.product = product
+      entry.order = shipped_order
+      entry.quantity = 1
+      entry.save
+
+      post ship_path(entry.id)
+      assert_redirected_to order_fulfillment_path
+    end
+  end
 
 
   end
