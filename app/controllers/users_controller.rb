c8f78965803a8dcc0b17541f63dbddb6fcbcc717
murog <guillermina.muro@gmail.com>
2017-10-27 03:44:29 -0700

    commented out actions that are not being used

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :permission, except: [:index, :show]
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
   def index
     @users = []
@@ -25,29 +25,29 @@ class UsersController < ApplicationController
     end
   end
 
-  def create
-    @user = User.new user_params
+  # def create
+  #   @user = User.new user_params
+  #
+  #   if @user.save
+  #     flash[:success] = "User added successfully"
+  #     redirect_to root_path
+  #   else
+  #     flash.now[:error] = "User not added"
+  #     render :new
+  #   end
+  # end
 
-    if @user.save
-      flash[:success] = "User added successfully"
-      redirect_to root_path
-    else
-      flash.now[:error] = "User not added"
-      render :new
-    end
-  end
+  # def login
+  # end
 
-  def login
-  end
-
-  def update
-    @user = User.find_by(id: params[:id].to_i)
-    redirect_to root_path
-
-    unless @user
-      redirect_to users_path
-    end
-  end
+  # def update
+  #   @user = User.find_by(id: params[:id].to_i)
+  #   redirect_to root_path
+  #
+  #   unless @user
+  #     redirect_to users_path
+  #   end
+  # end
 
   def profile
     @user = @user
@@ -77,9 +77,9 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    return params.require(:users).permit(:username, :email)
-  end
+  # def user_params
+  #   return params.require(:users).permit(:username, :email)
+  # end
 
   def permission
     if !(@user)
